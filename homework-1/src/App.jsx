737c517dfa34e5d4c19a92d31ab3661bc1464dea
Alisher <abasalisher12@gmail.com>
2025-09-08 19:07:10 +0500

    Homework-1

---
+++ homework-1/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <h1>Hello, React!</h1>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ homework-1/src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header className="header">
+    <p className="logo">My Website</p>
+    <nav>
+        <ul className="nav">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Profile</a></li>
+        </ul>
+    </nav>
+    </header>
+  )
+}
+
+export default Header
